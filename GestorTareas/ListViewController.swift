a99ca0953c4f5b078d0207d3c6260956961f4a58
José Gabriel Ferrer Morales <jgabriel.ferrer@gmail.com>
2019-03-10 16:28:26 +0100

    2.3. Programando nuestra primera pantalla

--- GestorTareas/ListViewController.swift
+++ GestorTareas/ListViewController.swift
@@ -10,8 +10,50 @@ import UIKit
 
 class ListViewController: UITableViewController {
 
-    @IBAction func addTask(_ sender: UIBarButtonItem) {
+    var taskManager = TaskManager.sharedInstance
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskManager.tasks.count
     }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellIdentifier", for: indexPath) as! TaskCell
+        let task = taskManager.tasks[indexPath.row]
+        
+        cell.lblTask.text = task["title"]
+        
+        if let image = task["icon"] {
+            cell.imgTask.image = UIImage(named: image)
+        } else {
+            cell.imgTask.image = UIImage(named: "icon-no-icon")
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80.0
+    }
+    
+    @IBAction func addTask(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Nueva tarea", message: "Nombre de la tarea", preferredStyle: .alert)
+        alertController.addTextField(configurationHandler: nil)
+        
+        let alertAction = UIAlertAction(title: "Guardar", style: .default) { (alertAction) in
+            if let textFieldText = alertController.textFields?.first?.text {
+                self.taskManager.tasks.append(["title": textFieldText])
+                
+                self.tableView.reloadData()
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        alertController.addAction(alertAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
     
 }
